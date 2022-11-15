3f66ff660eaa05ec9690e28d8e8c1f422913d71d
nasimulsakib <113965505+nasimulsakib@users.noreply.github.com>
2022-11-15 12:57:35 +0600

    Add files via upload

---
+++ lab 7,problem1.cpp
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<ctype.h>
+
+char stack[100];
+int top = -1;
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+
+char pop()
+{
+    if(top == -1)
+        return -1;
+    else
+        return stack[top--];
+}
+
+int order(char x)
+{
+    if(x == '(')
+        return 0;
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
+    return 0;
+}
+
+int main()
+{
+    char expression[100];
+    char *e, x;
+    printf("Expression : ");
+    scanf("%s",expression);
+    printf("\n");
+    e = expression;
+
+    while(*e != '\0')
+    {
+        if(isalnum(*e))
+            printf("%c ",*e);
+        else if(*e == '(')
+            push(*e);
+        else if(*e == ')')
+        {
+            while((x = pop()) != '(')
+                printf("%c ", x);
+        }
+        else
+        {
+            while(order(stack[top]) >= order(*e))
+                printf("%c ",pop());
+            push(*e);
+        }
+        e++;
+    }
+
+    while(top != -1)
+    {
+        printf("%c ",pop());
+    }return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
